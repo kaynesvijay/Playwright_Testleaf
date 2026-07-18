62ab0beb2e4130d40b0fedb844aeccaf54898055
vijaykumar <kaynesvijay@gmail.com>
2026-07-18 16:46:51 +0530

    Day1 commit

---
+++ sample.js
@@ -0,0 +1,31 @@
+console.log('Hello World')
+console.log('Vijay')
+ for (let i = 0; i < 5; i++) {
+     console.log('Vinayaka')
+ }
+firstName = 'kumar'
+console.log(firstName)
+var emp = 1234
+console.log(emp)
+var insur=true
+console.log(insur)
+var notInsur
+console.log(notInsur)
+var vehicle = null
+console.log(vehicle)
+console.log(typeof firstName, typeof emp, typeof insur, typeof notInsur, typeof vehicle)
+console.log('Number of characters in firstName:', firstName.length)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+var a=10
+var b=20
+console.log('a=b', a==b)
+console.log('a!=b', a!=b)
+console.log('a>b', a>b)
+console.log('a==b', a==b)
+console.log(a==b)
+console.log(a)
+var firstWord = 'Ram'
+var secondWord = 'Kumar'
+console.log(firstWord + ' ' + secondWord)
+console.log(firstWord +  secondWord)
